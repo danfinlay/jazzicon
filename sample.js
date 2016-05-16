b701861b187e07351b195bd980fbef52bfc68823
Dan Finlay <dan@danfinlay.com>
2016-05-16 00:29:50 -0700

    More samples

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 var jazzicon = require('./')
 
 var body = document.querySelector('body')
-for(var i = 0; i < 18; i++) {
+for(var i = 0; i < 100; i++) {
   var el = jazzicon(100, i)
   body.appendChild(el)
 }
