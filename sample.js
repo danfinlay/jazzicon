18f33738003de83cdc280f588bc681eaef172068
Dan Finlay <dan@danfinlay.com>
2016-10-26 15:44:55 -0700

    Make sample deterministic

--- sample.js
+++ sample.js
@@ -2,6 +2,6 @@ var jazzicon = require('./')
 
 var body = document.querySelector('body')
 for(var i = 0; i < 600; i++) {
-  var el = jazzicon(100, Math.round(Math.random() * 10000000))
+  var el = jazzicon(100, i)
   body.appendChild(el)
 }
